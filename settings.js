3341839548c5d46d9783f031e31d5696440d9193
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-12 15:07:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
+  global.ownername = 'Rull' // Ganti Jadi Namamu
+  global.owner = ['6285746030186','6285746030186','6285746030186'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285746030186' // Owner Utama Istilahnya
+  global.botname = 'MoonBot' // Ganti Jadi Nama Botmu
+  global.packname = 'MoonBot' // Sticker Weem
+  global.author = 'Moon' // Sticker Weem
+  global.webme = 'https://nekopoi.care' // Ganti Sama Web Mu
   global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/rullebinaru' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -63,7 +63,7 @@
   private: '_Khusus Private Chat_',
   bot: '_Khusus Pengguna Nomer Bot_',
   wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  done: '_Done 🌙_',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
